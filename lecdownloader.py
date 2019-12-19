adebd513b7f80217b851b70ea34e4f97716f312b
Colton Carner <ccarner13@gmail.com>
2019-12-19 21:00:11 +0200

    updated script and readme

--- lecdownloader.py
+++ lecdownloader.py
@@ -1,90 +1,109 @@
 """
 Script for downloading lectures from an echo360 page
-made in august 2019
+Made in august 2019 + updated in december 2019.
 
-Takes the URL, username (email), password as required cmd line args.
-Takes optional 4th command line arg for number of initial videos to skip 
-(eg if script didn't complete previously, can start again where left off).
-
-Uses chromedriver (make sure its available in PATH and appropriate version for chrome version installed)
+See readme or use -h flag for more information
 """
-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import sys
 import time
+import os 
+import argparse
 
-try:
-    assert len(sys.argv>=4)
-except AssertionError as e:
-    print("please include at least 3 cmd line args: URL, username/email, pw")
-    print(e)
+"""========================ARGUMENTS======================================="""
+parser = argparse.ArgumentParser()
 
-#full URL of the echo360page
-url = sys.argv[1]
-username = sys.argv[2]
-password = sys.argv[3]
+#positional, required args first
+parser.add_argument('url', type=str, help="Url of echo360 lecture page")
+parser.add_argument('echo360Email',  type=str, help='email used to log into echo360, need to make an echo360 account and link to unimelb')
+parser.add_argument('uomUsername', type=str, help="Unimelb SSO username")
+parser.add_argument('uomPassword', type=str, help="Unimelb SSO password")
 
-#set skipVideos if we've stated we want to skip some by giving a 4th cmd line arg
-skipVideos = 0
-if len(sys.argv)>4:
-    skipVideos = int(sys.argv[4])
+#now the optional flags
+parser.add_argument('-d', metavar="downloadLocation", dest='downloadLocation', default="downloadedLectures", type=str, help="location to save lectures (relative to download folder)" )
+parser.add_argument('-w', metavar="waitTime", dest='waitTime', default=3, type=int, help="time to wait in seconds between actions (default 3s, make longer for slower connections)")
+parser.add_argument('-s', metavar="skipNum", dest='skipNum', default=0, type=int, help="number of lectures to skip(default 0, use if script previously completed partially to skip the first 's' lectures)")
 
-driver = webdriver.Chrome()
-driver.get(url)
+args = parser.parse_args()
+
+
+"""========================Webdriver======================================="""
+chromeOptions = webdriver.ChromeOptions()
+#setting up location for custom download folder if specified
+prefs = {"download.default_directory" : "C:\\Users\\colto\\Downloads\\"+ args.downloadLocation}
+chromeOptions.add_experimental_option("prefs",prefs)
+driver = webdriver.Chrome( options=chromeOptions)
+
+print("Initialised driver")
+
+
+"""======================== Navigate to page + Login ======================================="""
+driver.get(args.url)
 
 #login page
 assert "Email" in driver.title
+print ("Arrived at echo360 email page")
 elem = driver.find_element_by_id("email")
 elem.clear()
-elem.send_keys("username")
+elem.send_keys(args.echo360Email)
 elem.send_keys(Keys.RETURN)
+
+# need to sleep so whole page loads, otherwise elements not found since loads dynamically
+time.sleep(args.waitTime)
 
 #unimelb sso page
 assert "Melbourne" in driver.title
+print("Arrived at unimelb SSO page")
 elem = driver.find_element_by_id("usernameInput")
 elem.clear()
-elem.send_keys("ccarner")
+elem.send_keys(args.uomUsername)
 elem = driver.find_element_by_id("passwordInput")
-elem.send_keys("password")
+elem.send_keys(args.uomPassword)
 elem.send_keys(Keys.RETURN)
+
+time.sleep(args.waitTime)
 
 #now at the echo360 page
 assert "Home" in driver.title
+print("Arrived at Echo360 lecture recording page")
 
-# need to sleep so whole page loads, otherwise elements not found since loads dynamically
-time.sleep(2)
+time.sleep(args.waitTime)
+
+"""========================Downloading Videos======================================="""
 
 videos = driver.find_elements_by_css_selector(".menu-opener")
 #note, equivalent to videos = driver.find_elements_by_class_name("menu-opener")
 
 print("Located {} videos".format(len(videos)))
-if skipVideos != 1:
-    print ("Skipping {} videos".format(skipVideos))
+if args.skipNum != 0:
+    print ("Skipping {} videos".format(args.skipNum))
 
 print("Downloading videos:")
 for (videoNum,video) in enumerate(videos,1):
-    if videoNum > skipVideos:
+    if videoNum > args.skipNum:
         #don't skip these videos
         video.click()
-        time.sleep(1)
+        time.sleep(args.waitTime)
         downloadButton = driver.find_element_by_xpath("//*[contains(text(), 'Download original')]")
         downloadButton.click()
-        time.sleep(1)
+        time.sleep(args.waitTime)
         qualitySelector = driver.find_element_by_class_name('select-wrapper')
         qualitySelector.click()
-        time.sleep(1)
+        time.sleep(args.waitTime)
         selectionFocus = driver.switch_to.active_element
         selectionFocus.send_keys(Keys.ARROW_DOWN)
-        time.sleep(1)
+        time.sleep(args.waitTime)
         selectionFocus.send_keys(Keys.RETURN)
-        time.sleep(1)
+        time.sleep(args.waitTime)
         downloadButton = driver.find_element_by_class_name("downloadBtn")
         downloadButton.click()
-        time.sleep(1)
+        time.sleep(args.waitTime)
         print(" " + str(videoNum), end ="")
+    else:
+        print ("Skipped video # {}".format(videoNum))
         
-print("\nCompleted, downloaded a total of {} videos".format(len(videos)-skipVideos))
+print("\nCompleted, downloaded a total of {} videos".format(len(videos)-args.skipNum))
 quit()
 
 #don't want to close the driver... leave open to ensure downloads continue
--- readme.md
+++ readme.md
@@ -1,6 +1,28 @@
+# Lecture Download Script
 Simple script for downloading lectures from echo360 for university of melbourne students.
 
 Requires Python 3.5+ with Selenium Webdriver package, google chrome, appropriate chromedriver.
+Webdriver can be plaed in PATH or else in same folder as python script.
 
-Usage:
-lectureDownloader [URL of echo360 lecture page] [LMSusername][LMSpassword] [Optional: number of lectures to skip downloading from the start, useful if script didn't complete previously]
+Need to created an echo360 account, and link it to your unimelb account (allows you to login to echo360 without going through the LMS)
+
+## Usage:
+For more info on usage, use the --help flag  (eg python lecturedownloader.py --help)
+
+### Positional args (required)
+lectureDownloader [URL of echo360 lecture page][echo360 email][unimelbUsername][unimelbPassword]
+
+### Flags (optional)
+-d :    Download location (relative to downloads folder, eg specify a new folder to create)
+-w :    Time to wait (seconds) between webdriver actions (defaults to 3 seconds, extend if              slower connections)
+-s  :   Number of initial lectures to skip downloading (useful if a previous execution was              interrupted)
+
+### Example Usage
+
+`python lecdownloader.py https://echo360.org.au/section/90f4ef0a-fd08-4efb-9c9e-b7ba9a5e0150/home ccarner13@gmail.com ccarner COLTONSPW01 -d securityLectures -w 5`
+- Downloads from echo360 page located at https://echo360.org.au/section/90f4ef0a-fd08-4efb-9c9e-b7ba9a5e0150/home
+- Has an Echo360 account set up associated with the email ccarner13@gmail.com
+- Unimelb Username of ccarner
+- Unimelb Password of COLTONSPW01
+- Saving to downloads/securityLectures
+- Waits for 5 seconds between clicks
